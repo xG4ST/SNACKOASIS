3d68191b0d70dc2d79732b12e51ed1f17a02d1b9
xG4ST <gus64_gast@outlook.es>
2020-05-14 15:25:51 -0500

    aber

--- Proyecto_OASIS/Proyecto_OASIS/RegistrarProductos.Designer.cs
+++ Proyecto_OASIS/Proyecto_OASIS/RegistrarProductos.Designer.cs
@@ -103,7 +103,7 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("BigNoodleTitling", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(0)))), ((int)(((byte)(109)))));
-            this.label3.Location = new System.Drawing.Point(85, 133);
+            this.label3.Location = new System.Drawing.Point(102, 132);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(385, 70);
             this.label3.TabIndex = 21;
