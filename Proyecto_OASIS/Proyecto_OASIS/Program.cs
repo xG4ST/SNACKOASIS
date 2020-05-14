bbed4e96f53b351a18c9233c59fd5280c2fee6fe
xG4ST <gus64_gast@outlook.es>
2020-05-14 13:44:42 -0500

    f

--- Proyecto_OASIS/Proyecto_OASIS/Program.cs
+++ Proyecto_OASIS/Proyecto_OASIS/Program.cs
@@ -16,7 +16,7 @@ namespace Proyecto_OASIS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Juego());
+            Application.Run(new Login());
         }
     }
 }
