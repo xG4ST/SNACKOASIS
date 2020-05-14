1aaaccf1c572ba167da07b588baf87c6059786cd
Luis Guerrero <manuel_guerreroalns@uanl.edu.mx>
2020-05-14 18:41:45 -0500

    Sin errores

--- Proyecto_OASIS/Proyecto_OASIS/Program.cs
+++ Proyecto_OASIS/Proyecto_OASIS/Program.cs
@@ -16,7 +16,7 @@ namespace Proyecto_OASIS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new RegistrarProovedor());
+            Application.Run(new Login());
         }
     }
 }
