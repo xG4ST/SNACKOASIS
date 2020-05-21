084698e3e40e362781b064c3059ba05c46ae2983
xG4ST <gus64_gast@outlook.es>
2020-05-20 23:55:29 -0500

    Todoeso

--- Proyecto_OASIS/Proyecto_OASIS/RegistrarProductos.Designer.cs
+++ Proyecto_OASIS/Proyecto_OASIS/RegistrarProductos.Designer.cs
@@ -166,7 +166,7 @@
             this.lbhora.BackColor = System.Drawing.Color.Transparent;
             this.lbhora.Font = new System.Drawing.Font("BigNoodleTitling", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbhora.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(194)))), ((int)(((byte)(149)))));
-            this.lbhora.Location = new System.Drawing.Point(1272, 9);
+            this.lbhora.Location = new System.Drawing.Point(1050, 9);
             this.lbhora.Name = "lbhora";
             this.lbhora.Size = new System.Drawing.Size(130, 70);
             this.lbhora.TabIndex = 28;
